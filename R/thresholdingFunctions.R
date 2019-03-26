c3447ff108d2e0a52b9daf41693a2d4f74fc7ce4
Xingchi Li <38049831+Kay-Xingchi@users.noreply.github.com>
2019-03-26 14:43:40 -0500

    remove redundant code

--- R/thresholdingFunctions.R
+++ R/thresholdingFunctions.R
@@ -11,22 +11,3 @@
 soft <- function(a, lambda) {
 	sign(a) * max(abs(a) - lambda, 0)
 }
-
-
-#' Hard-thresholding function
-#'
-#' @param a scalar to be tresholded
-#' @param lambda threshold value, should be non-negative
-#'
-#' @return Hard-tresholded value of a
-#' @export
-#'
-#' @examples
-#' hard(3,1)
-hard <- function(a, lambda) {
-  if(abs(a) > lambda) {
-    a
-  } else {
-    0
-  }
-}
