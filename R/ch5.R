e72900b097a271d6f20b98a5c3a4ea80f5d03ba6
Xingchi Li <38049831+Kay-Xingchi@users.noreply.github.com>
2019-04-03 19:55:14 -0500

    added basic functionality of normal_gamma_conjugate_family

--- R/ch5.R
+++ R/ch5.R
@@ -1,24 +1,16 @@
 normal_gamma_conjugate_family(sample_size = 10000, mu_0 = 5, sigma_0_sequare = 4, kappa_0 = 1, nu_0 = 2, y_1 = c(2.11,9.75,13.88,11.3,8.93,15.66,16.38,4.54,8.86,11.94,12.47,11.11,11.65,14.53,9.61,7.38,3.34,9.06,9.45,5.98,7.44,8.5,1.55,11.45,9.73), confidence_interval = c(0.025, 0.975))
 
-sample_size <- 10000
-confidence_interval <- c(0.025, 0.975)
-
-hours1=c(2.11,9.75,13.88,11.3,8.93,15.66,16.38,4.54,8.86,11.94,12.47,11.11,11.65,14.53,9.61,7.38,3.34,9.06,9.45,5.98,7.44,8.5,1.55,11.45,9.73)
-
-mu_0 <- 5
-sigma_0_sequare <- 4
-kappa_0 <- 1
-nu_0 <- 2
-
-y_bar.1 <- mean(hours1)
-y_var.1 <- var(hours1) 
-y_sd.1 <- sd(hours1)
-length.1 <- length(hours1)
-
-kappa_n.1 <- kappa_0 + length.1
-mu_n.1 <- (kappa_0 * mu_0 + length.1 * y_bar.1) / kappa_n.1 
-nu_n.1 <- nu_0 + length.1 
-sigma_n_square.1 <- (1 / nu_n.1) * (nu_0 * sigma_0_sequare + (length.1 - 1) * y_var.1 + ((kappa_0 * length.1) / kappa_n.1) * (y_bar.1 - mu_0)^2)
+normal_gamma_conjugate_family <- function(sample_size, mu_0, sigma_0_sequare, kappa_0, nu_0, y_1, y_2, confidence_interval) {
+	y_bar.1 <- mean(y_1)
+	y_var.1 <- var(y_1) 
+	y_sd.1 <- sd(y_1)
+	length.1 <- length(y_1)
+	kappa_n.1 <- kappa_0 + length.1
+	mu_n.1 <- (kappa_0 * mu_0 + length.1 * y_bar.1) / kappa_n.1 
+	nu_n.1 <- nu_0 + length.1 
+	sigma_n_square.1 <- (1 / nu_n.1) * (nu_0 * sigma_0_sequare + (length.1 - 1) * y_var.1 + ((kappa_0 * length.1) / kappa_n.1) * (y_bar.1 - mu_0)^2)
+	
+}
 
 # sampling from the posterior distribution
 sigma_square_inverse.1 <- rgamma(sample_size, nu_n.1 / 2, nu_n.1*sigma_n_square.1 / 2) 
