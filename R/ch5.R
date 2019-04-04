f176827df0f64297cde86375b06cb4e590d4836f
Xingchi Li <38049831+Kay-Xingchi@users.noreply.github.com>
2019-04-04 10:09:46 -0500

    added data of y_3

--- R/ch5.R
+++ R/ch5.R
@@ -1,6 +1,6 @@
 normal_gamma_conjugate_family(sample_size = 10000, mu_0 = 5, sigma_0_sequare = 4, kappa_0 = 1, nu_0 = 2, y_1 = c(2.11,9.75,13.88,11.3,8.93,15.66,16.38,4.54,8.86,11.94,12.47,11.11,11.65,14.53,9.61,7.38,3.34,9.06,9.45,5.98,7.44,8.5,1.55,11.45,9.73), confidence_interval = c(0.025, 0.975))
 
-normal_gamma_conjugate_family <- function(sample_size, mu_0, sigma_0_sequare, kappa_0, nu_0, y_1, y_2, confidence_interval) {
+normal_gamma_conjugate_family <- function(sample_size, mu_0, sigma_0_sequare, kappa_0, nu_0, y_1, y_2, y_3, confidence_interval) {
 	y_bar.1 <- mean(y_1)
 	y_var.1 <- var(y_1) 
 	y_sd.1 <- sd(y_1)
