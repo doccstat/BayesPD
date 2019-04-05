9d65d96b9e32cfe63c6b0e9aef660f07142c8a9f
Xingchi Li <38049831+Kay-Xingchi@users.noreply.github.com>
2019-04-05 09:51:49 -0500

    added skeleton of sensitivity analysis

---
+++ R/sensitivity_analysis.R
@@ -0,0 +1,4 @@
+sensitivity_analysis(sample_size = 10000, y_bar.1 = 75.2, standard_deviation.1 = 7.3, y_bar.2 = 77.5, standard_deviation.2 = 8.1)
+sensitivity_analysis <- function(sample_size, y_bar.1, standard_deviation.1, y_bar.2, standard_deviation.2) {
+	
+}
