a6f94ab1dde779aa5be078991f7c75ed796c5c0f
Xingchi Li <38049831+Kay-Xingchi@users.noreply.github.com>
2019-04-07 15:55:59 -0500

    added comparison parameter to determine which way should the program compute the probability

--- R/sensitivity_analysis.R
+++ R/sensitivity_analysis.R
@@ -15,7 +15,7 @@
 #' @export
 #'
 #' @examples sensitivity_analysis(sample_size = 10000, y_bar.1 = 75.2, standard_deviation.1 = 7.3, y_bar.2 = 77.5, standard_deviation.2 = 8.1, mu_0 = 75, sigma_0_square = 100, n = 16, kappa_0 = c(1,2,4,8,16,32), nu_0 = c(1,2,4,8,16,32)) # sigma_0_square = 4?
-sensitivity_analysis <- function(sample_size, y_bar.1, standard_deviation.1, y_bar.2, standard_deviation.2, mu_0, sigma_0_square, n, kappa_0, nu_0) {
+sensitivity_analysis <- function(sample_size, y_bar.1, standard_deviation.1, y_bar.2, standard_deviation.2, mu_0, sigma_0_square, n, kappa_0, nu_0, theta.1_smaller_than_theta.2 = TRUE) {
 	probability <- rep(0,length(kappa_0))
 
 	for(i in 1:length(kappa_0)) {
@@ -37,4 +37,9 @@ sensitivity_analysis <- function(sample_size, y_bar.1, standard_deviation.1, y_b
 		sigma_square_inverse.2 <- rgamma(sample_size, nu_n / 2, nu_n * sigma_n_square.2 / 2)
 		sigma_square.2 <-  1 / sigma_square_inverse.2
 		theta.2 <- rnorm(sample_size, mu_n.2, sqrt(sigma_square.2 / kappa_n))
+		if(theta.1_smaller_than_theta.2) {
+			probability[i] <- mean(theta.1 < theta.2)
+		} else {
+			probability[i] <- mean(theta.1 > theta.2)
+		}
 }
