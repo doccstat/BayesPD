a9c0537ea34d5870e67dd5d736249b379ad21a1d
Xingchi Li <38049831+Kay-Xingchi@users.noreply.github.com>
2019-04-07 15:57:37 -0500

    added plot functionality

--- R/sensitivity_analysis.R
+++ R/sensitivity_analysis.R
@@ -15,7 +15,7 @@
 #' @export
 #'
 #' @examples sensitivity_analysis(sample_size = 10000, y_bar.1 = 75.2, standard_deviation.1 = 7.3, y_bar.2 = 77.5, standard_deviation.2 = 8.1, mu_0 = 75, sigma_0_square = 100, n = 16, kappa_0 = c(1,2,4,8,16,32), nu_0 = c(1,2,4,8,16,32)) # sigma_0_square = 4?
-sensitivity_analysis <- function(sample_size, y_bar.1, standard_deviation.1, y_bar.2, standard_deviation.2, mu_0, sigma_0_square, n, kappa_0, nu_0, theta.1_smaller_than_theta.2 = TRUE) {
+sensitivity_analysis <- function(sample_size, y_bar.1, standard_deviation.1, y_bar.2, standard_deviation.2, mu_0, sigma_0_square, n, kappa_0, nu_0, theta.1_smaller_than_theta.2 = TRUE, plot = TRUE) {
 	probability <- rep(0,length(kappa_0))
 
 	for(i in 1:length(kappa_0)) {
@@ -42,4 +42,10 @@ sensitivity_analysis <- function(sample_size, y_bar.1, standard_deviation.1, y_b
 		} else {
 			probability[i] <- mean(theta.1 > theta.2)
 		}
+	}
+
+	if(plot) {
+		plot(kappa_0, probability, main = "Sensitivity analysis", xlab = "kappa_0 = nu_0")
+	}
+	
 }
