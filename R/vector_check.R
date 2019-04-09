dac3cae2d6b80c487aa3a3b5d2618f404e593432
Xingchi Li <38049831+Kay-Xingchi@users.noreply.github.com>
2019-04-09 18:08:51 -0500

    roxygen skeleton added for the function vector_check

--- R/vector_check.R
+++ R/vector_check.R
@@ -1,4 +1,11 @@
-
+#' Title Check if the object is a vector or not
+#'
+#' @param object Vector to be tested.
+#' @param length Expected length.
+#'
+#' @return True or False. Vector of length 1 is not treated as a vector, thus returning false, unless the specified length is 1.
+#' @export
+#'
 vector_check <- function(object, length = NULL) {
 	if(is.null(object)) { stop("The variable provided is null.") }
 	if(is.null(length) && is.vector(object) && length(object) > 1) { return(TRUE) }
