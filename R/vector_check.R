c51f10cca24e76c00158365457f21ea07c4dec61
Xingchi Li <38049831+Kay-Xingchi@users.noreply.github.com>
2019-04-09 18:07:38 -0500

    check object is a vector of length bigger than 1 (or else a number)

--- R/vector_check.R
+++ R/vector_check.R
@@ -1,4 +1,5 @@
 
 vector_check <- function(object, length = NULL) {
 	if(is.null(object)) { stop("The variable provided is null.") }
+	if(is.null(length) && is.vector(object) && length(object) > 1) { return(TRUE) }
 }
