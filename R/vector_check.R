87283b58edf5bb8a80904d248d151b199ce4e180
Xingchi Li <38049831+Kay-Xingchi@users.noreply.github.com>
2019-04-09 18:09:07 -0500

    all kinds of examples for the function vector_check

--- R/vector_check.R
+++ R/vector_check.R
@@ -6,6 +6,11 @@
 #' @return True or False. Vector of length 1 is not treated as a vector, thus returning false, unless the specified length is 1.
 #' @export
 #'
+#' @examples vector_check(object = c(1)) == FALSE
+#' vector_check(object = c(1), length = 1) == TRUE
+#' vector_check(object = c(1, 1)) == TRUE
+#' vector_check(object = c(1, 2), length = 2) == TRUE
+#' vector_check(object = c(1, 1), length = 1) == FALSE
 vector_check <- function(object, length = NULL) {
 	if(is.null(object)) { stop("The variable provided is null.") }
 	if(is.null(length) && is.vector(object) && length(object) > 1) { return(TRUE) }
