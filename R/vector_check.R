11ef44f2748d53961047d3c9e9e2e3147815d87e
Xingchi Li <38049831+Kay-Xingchi@users.noreply.github.com>
2019-04-09 18:08:31 -0500

    all other cases will return false

--- R/vector_check.R
+++ R/vector_check.R
@@ -3,4 +3,5 @@ vector_check <- function(object, length = NULL) {
 	if(is.null(object)) { stop("The variable provided is null.") }
 	if(is.null(length) && is.vector(object) && length(object) > 1) { return(TRUE) }
 	if(!is.null(length) && is.vector(object) && length(object) == length) { return(TRUE) }
+	return(FALSE)
 }
