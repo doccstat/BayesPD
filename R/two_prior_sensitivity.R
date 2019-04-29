9410da025e90cfcc0b7a52241720961992d2f997
Xingchi Li <38049831+Kay-Xingchi@users.noreply.github.com>
2019-04-28 22:32:24 -0500

    compute the different length before analyze the sensitivity

--- R/two_prior_sensitivity.R
+++ R/two_prior_sensitivity.R
@@ -15,4 +15,9 @@
 #' two_prior_sensitivity(sample_size = 5000, gamma_theta_a = 2, gamma_theta_b = 1,
 #' y.1 = menchild30bach, y.2 = menchild30nobach,
 #' gamma_gamma_a = 2^(seq(3, 7)), gamma_gamma_b = 2^(seq(3, 7)))
+two_prior_sensitivity <- function(sample_size, gamma_theta_a, gamma_theta_b, y.1, y.2, gamma_gamma_a, gamma_gamma_b) {
+	# Since the two vectors can have different length, we compute each of them.
+	n.1 <- length(y.1)
+	n.2 <- length(y.2)
+	# Create a vector to reduce memory cost.
 }
