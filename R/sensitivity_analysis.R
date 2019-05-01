bb0ea2a6fceac0e26b619e864a8ec8132ab24005
Xingchi Li <38049831+Kay-Xingchi@users.noreply.github.com>
2019-04-30 21:07:33 -0500

    Use data provided by the package

--- R/sensitivity_analysis.R
+++ R/sensitivity_analysis.R
@@ -19,7 +19,7 @@
 #' @examples 
 #' sensitivity_analysis(sample_size = 10000, y_bar.1 = 75.2, standard_deviation.1 = 7.3, 
 #' y_bar.2 = 77.5, standard_deviation.2 = 8.1, mu_0 = 75, sigma_0_square = 100, 
-#' n = 16, kappa_0 = c(1,2,4,8,16,32), nu_0 = c(1,2,4,8,16,32)) # sigma_0_square = 4?
+#' n = 16, kappa_0 = c(1,2,4,8,16,32), nu_0 = c(1,2,4,8,16,32))
 sensitivity_analysis <- function(sample_size, y_bar.1, standard_deviation.1, y_bar.2, standard_deviation.2, mu_0, sigma_0_square, n, kappa_0, nu_0, theta.1_smaller_than_theta.2 = TRUE, plot = TRUE) {
 
 	if(sample_size %% 1 != 0 || sample_size <= 0) {
--- R/two_prior_sensitivity.R
+++ R/two_prior_sensitivity.R
@@ -26,11 +26,11 @@ two_prior_sensitivity <- function(sample_size, gamma_theta_a, gamma_theta_b, y.1
 		thetas <- gammas <- numeric(sample_size)
 		
 		# Since the computation of theta is based on gamma, we need the initial value which is not in the vector, and we need to take it into consideration alone.
-		thetas[1] <- rgamma(1, sum(y.1 + y.2) + gamma_theta_a, n.1 + n.2 * mean(y.2)/mean(y.1) + gamma_theta_b)
+		thetas[1] <- rgamma(1, sum(y.1) + sum(y.2) + gamma_theta_a, n.1 + n.2 * mean(y.2)/mean(y.1) + gamma_theta_b)
 		gammas[1] <- rgamma(1, sum(y.2) + gamma_gamma_a[i], n.2 * thetas[1] + gamma_gamma_b[i])
 
 		for (j in 2:sample_size) {
-			thetas[j] <- rgamma(1, sum(y.1 + y.2) + gamma_theta_a, n.1 + n.2 * gammas[j-1] + gamma_theta_b)
+			thetas[j] <- rgamma(1, sum(y.1) + sum(y.2) + gamma_theta_a, n.1 + n.2 * gammas[j-1] + gamma_theta_b)
 			gammas[j] <- rgamma(1, sum(y.2) + gamma_gamma_a[i], n.2 * thetas[j] + gamma_gamma_b[i])
 		}
 
